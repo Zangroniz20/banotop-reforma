77209eefc38ecbffc82a39bafc03b84bbd04dcd5
Zangroniz20 <71570172+Zangroniz20@users.noreply.github.com>
2025-03-27 16:27:54 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BanotopLevels from "./BanotopLevels";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BanotopLevels />
+  </React.StrictMode>
+);
