919e78013801e3db9fa6617a9e49e5ab3b8beb5a
Zangroniz20 <71570172+Zangroniz20@users.noreply.github.com>
2025-03-31 12:09:10 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BanotopLevels from "./BanotopLevels";
-import BanotopAlicatado from "./BanotopAlicatado"; // 👈 importante importar
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BanotopLevels />
-    <BanotopAlicatado nivel="Esencial" /> {/* 👈 mostrar el componente directamente */}
   </React.StrictMode>
 );
