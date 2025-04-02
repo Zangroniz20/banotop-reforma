aa77130c8f386cb873a15f168a5725572df8833d
Zangroniz20 <71570172+Zangroniz20@users.noreply.github.com>
2025-04-02 18:20:25 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import HomeHero from "./HomeHero"; // 👈 importa el nuevo componente
 import BanotopLevels from "./BanotopLevels";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BanotopLevels />
+    <div>
+      <HomeHero />         {/* 👈 Aquí se muestra tu cabecera con fondo y logo */}
+      <BanotopLevels />    {/* 👈 Esto se queda como lo tenías */}
+    </div>
   </React.StrictMode>
 );
